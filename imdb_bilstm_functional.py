aad9ccc6d82aef6a82f07e11a0b6aa6113edb4b4
jonojace <jonojace@gmail.com>
2018-05-25 09:02:18 +0100

    Added functional implementation of imdb movie sentiment prediction

---
+++ imdb_bilstm_functional.py
@@ -0,0 +1,67 @@
+'''
+Task: IMDB movie reviews sentiment prediction
+Model: Bi-directional LSTM
+
+Each sentence in the training set is represented as a sequence of word indices
+Which are collapsed down to a 128-dim embedding space via an embedding layer
+Then are then fed one by one into a Bidirectional LSTM that encodes the entire sentence.
+The outputs of the Bidirectional LSTM are then fed into a single unit layer that is passed
+through a sigmoidal activation function that represents the probability of a given sentence being positive sentiment
+'''
+
+#imports
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Model
+from keras.layers import Input, Dense, Dropout, Embedding, LSTM, Bidirectional
+from keras.preprocessing import sequence
+from keras.datasets import imdb
+
+
+#set hyperparameters
+max_features = 20000 #set vocabulary size, words outside of this are given OOV token
+batch_size = 32 #how big SGD batches are
+maxlen = 100 #maximum length of sentences in words
+
+#get dataset
+#x_train is a list of lists
+#Outer list is a list of training examples
+#Inner list is a list of indices, where each index represents a word
+print('Loading data...')
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+print(len(x_train), 'train sequences')
+print(len(x_test), 'test sequences')
+
+#The input to a Keras model must be a tensor (for matrix multiplication efficiencies) 
+#so must pad or truncate sentences to max length (in words)
+#also speeds up batch processing, it will arrange batches where sequences are same length
+print('Pad sequences (samples x time)')
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen) #returns numpy array
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+y_train = np.array(y_train) #keras only accepts numpy arrays, not python lists
+y_test = np.array(y_test)
+x_train = np.array(x_train)
+x_test = np.array(x_test)
+
+#instantiate a model by using Input and Output tensors, rather than Sequential()
+inputs = Input(shape=(maxlen,)) #maxlen number of words per training example
+
+#tensors are passed through the layers 
+x = Embedding(max_features, 128)(inputs) #embeddings are 128 dim vectors
+x = Bidirectional(LSTM(64))(x) #LSTM layer has 64 units
+x = Dropout(0.5)(x) #what proportion of inputs to set to 0
+predictions = Dense(1, activation='sigmoid')(x) #single sigmoidal output, predicting either 0 or 1, negative or positive sentiment
+
+#create the model that includes the various layers, including the input layer
+model = Model(inputs=inputs, outputs=predictions)
+model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+
+#train the model
+print('Train...')
+hist = model.fit(x_train, y_train,
+		  batch_size=batch_size,
+		  epochs=1,
+		  validation_data=[x_test, y_test]) 
+		  #using validation_data means don't have to call model.evaluate on test set to get acc on test data
